c322dba541471097c6cb9adaeed163b21d785f15
AliiAnr <alianur043@gmail.com>
2023-11-25 20:51:39 +0800

    Typing_Test

---
+++ Modul_4/src/Main.java
@@ -0,0 +1,28 @@
+import API.Database;
+import javax.swing.*;
+import java.awt.*;
+import java.sql.SQLException;
+
+public class Main {
+   public static void main(String[] args) {
+      try {
+         new Database();
+      } catch (SQLException | ClassNotFoundException e) {
+         e.printStackTrace();
+      }
+
+      LoginContainer loginContainer = new LoginContainer();
+      JPanel panel = new JPanel();
+      JFrame frame = new JFrame("Typing");
+
+      panel.setLayout(new GridBagLayout());
+      panel.add(loginContainer);
+      panel.setBackground(Color.WHITE);
+      frame.add(panel);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setSize(750, 700);
+      frame.setLocationRelativeTo(null);
+      frame.setVisible(true);
+
+   }
+}
